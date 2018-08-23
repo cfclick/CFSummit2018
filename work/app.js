9067c20c385d72e5ae6a540288759429889fe0e2
Shirak <shirakavakian@gmail.com>
2018-08-22 22:38:32 -0700

    new client btn

--- work/app.js
+++ work/app.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+    
+     /****************** Server ******************/
 
     $('#sync_btn').off('click').on('click', function(target){
 
@@ -130,6 +132,142 @@ $(document).ready(function(){
             console.log(xhr);
             $('#async_result_div').html(statusText);
             $("#total_async_await_duration").html("");
+        });
+        
+    });
+
+    /****************** Client ******************/
+
+    $('#client_sync_btn').off('click').on('click', function(target){
+
+        $.ajax({
+            type: "get",
+            url: "http://shirak-dell/CFSummit2018/work/UrlHelper.cfc?method=startWebSiteDownloadSync",
+            
+            beforeSend: function (xhr) {
+                $('#client_sync_result_div').html("<i class='fa fa-spinner fa-spin fa-3x fa-fw'></i>");
+                $("#client_total_sync_duration").html("");
+            },
+            async: true
+        }).done(function (response) {
+           // $('#sync_result_div').html(response);
+           result = JSON.parse(response);
+            if( result ){
+                totalDueration = 0;
+                str = "";
+                result.forEach(element => {
+
+                    str = str + 
+                    '<a href="#" class="list-group-item list-group-item-action flex-column align-items-start"><div class="d-flex w-100 justify-content-between"><h5 class="mb-1">' +
+                    element.url + '</h5><small>Char Len: ' + element.charLen + '</small></div><p class="mb-1">' + element.duration + ' mls</p><small>Is done? ' + element.isDone + '</small></a>';
+                    
+                    totalDueration += eval(element.duration);
+                    
+                    $("#client_total_sync_duration").html(totalDueration);
+                    /*str = str + "<li>url: " + element.url + "</li>" +
+                    "<li>len: " + element.charLen + "</li>" + 
+                    "<li>duration: " + element.duration + "</li>"; */
+                    
+                    $('#client_sync_result_div').html(str);
+                });
+            }
+            console.log(response);
+            
+        }).fail(function (xhr) {
+            console.log(xhr);
+            $('#client_sync_result_div').html("Sorry error check console log");
+            $("#client_total_sync_duration").html("");
+        });
+
+        //$('#sync_result_div').html("sync button clicked");
+    });
+
+    $('#client_async_btn').off('click').on('click', function(target){
+        $.ajax({
+            type: "get",
+            url: "http://shirak-dell/CFSummit2018/work/UrlHelper.cfc?method=startWebSiteDownloadASync",
+            
+            beforeSend: function (xhr) {
+                $('#client_async_result_div').html("<i class='fa fa-spinner fa-spin fa-3x fa-fw'></i>");
+                $("#client_total_async_duration").html("");
+            },
+            async: true
+        }).done(function (response) {
+            result = JSON.parse(response);
+            if( result ){
+                str = "";
+                totalDueration = 0;
+                totalPageDueration = 0;
+                grandTotalDueration = 0;
+                result.forEach(element => {
+                    
+                    str = str + 
+                    '<a href="#" class="list-group-item list-group-item-action flex-column align-items-start"><div class="d-flex w-100 justify-content-between"><h5 class="mb-1">' +
+                    element.url + '</h5><small>Char Len: ' + element.charLen + '</small></div><p class="mb-1">async duration: ' + element.duration + ' mls</p><small>page duration? ' + element.pageDuration + '</small></a>'
+                    
+                    totalDueration += eval(element.duration);
+                    totalPageDueration += eval(element.pageDuration);
+                    
+                    $("#client_total_async_duration").html(totalDueration);
+                    /*    str = str + "<li>url: " + element.url + "</li>" +
+                    "<li>len: " + element.charLen + "</li>" + 
+                    "<li>duration: " + element.duration + "</li>"; 
+                    */
+                    $('#client_async_result_div').html(str);
+                });
+            }
+            
+            console.log(response);
+
+        }).fail(function (xhr, type, statusText) {
+            console.log(xhr);
+            $('#client_async_result_div').html(statusText);
+            $("#client_total_async_duration").html("");
+        });
+        
+    });
+
+    $('#client_async_await_btn').off('click').on('click', function(target){
+        $.ajax({
+            type: "get",
+            url: "http://shirak-dell/CFSummit2018/work/UrlHelper.cfc?method=startWebSiteDownloadASyncAwait",
+            
+            beforeSend: function (xhr) {
+                $('#client_async_result_div').html("<i class='fa fa-spinner fa-spin fa-3x fa-fw'></i>");
+                $("#client_total_async_await_duration").html("");
+            },
+            async: true
+        }).done(function (response) {
+            result = JSON.parse(response);
+            if( result ){
+                str = "";
+                totalDueration = 0;
+                totalPageDueration = 0;
+                grandTotalDueration = 0;
+                result.forEach(element => {
+                    
+                    str = str + 
+                    '<a href="#" class="list-group-item list-group-item-action flex-column align-items-start"><div class="d-flex w-100 justify-content-between"><h5 class="mb-1">' +
+                    element.url + '</h5><small>Char Len: ' + element.charLen + '</small></div><p class="mb-1">async duration: ' + element.duration + ' mls</p><small>page duration? ' + element.pageDuration + '</small></a>'
+                    
+                    totalDueration += eval(element.duration);
+                    totalPageDueration += eval(element.pageDuration);
+                    
+                    $("#client_total_async_await_duration").html(totalDueration);
+                    /*    str = str + "<li>url: " + element.url + "</li>" +
+                    "<li>len: " + element.charLen + "</li>" + 
+                    "<li>duration: " + element.duration + "</li>"; 
+                    */
+                    $('#client_async_result_div').html(str);
+                });
+            }
+            
+            console.log(response);
+
+        }).fail(function (xhr, type, statusText) {
+            console.log(xhr);
+            $('#client_async_result_div').html(statusText);
+            $("#client_total_async_await_duration").html("");
         });
         
     });
